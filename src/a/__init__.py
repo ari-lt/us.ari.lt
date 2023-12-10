37179c453f64843c9a1fcdb7adc42dec18a9038d
Ari Archer <ari.web.xyz@gmail.com>
2023-12-10 13:10:23 +0200

    disable exception logging in server error
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- src/a/__init__.py
+++ src/a/__init__.py
@@ -7,7 +7,6 @@ import os
 import re
 import secrets
 import time
-import traceback
 from datetime import timedelta
 from functools import lru_cache
 from typing import Any, Dict, Optional, Tuple
@@ -297,8 +296,7 @@ def create_app(maria_user: str, maria_pass: str) -> flask.Flask:
                 "http.j2",
                 code=e.code,
                 summary=e.name.lower(),
-                description=(e.description or f"http error code {e.code}").lower()
-                + str(traceback.format_exc()),
+                description=(e.description or f"http error code {e.code}").lower(),
             ),
             e.code or 200,
         )
