840a68a6eb71e16aac83c406deab7113113a3cd9
Ari Archer <ari.web.xyz@gmail.com>
2024-01-25 00:49:49 +0200

    restar5t script rework
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -7,11 +7,11 @@ main() {
     systemctl restart nginx
     systemctl restart forgejo
 
-    su matrix -c 'cd ~/dendrite/ && ~/go/bin/dendrite --config dendrite.yaml & disown'
-    su mau -c 'cd ~/maubot/ && source ./bin/activate && python3 -m maubot & disown'
-    su us -c 'cd ~/us.ari.lt/ && source maria.env && source ./venv/bin/activate && ./scripts/run.sh & disown'
-    su voe -c 'cd ~/vim-or-emacs.ari.lt/ && source ./venv/bin/activate && ./scripts/run.sh & disown'
-    su searxng -c 'cd /usr/local/searxng/searxng-src && SEARXNG_SETTINGS_PATH=/etc/searxng/settings.yml python3 searx/webapp.py & disown'
+    su matrix -c 'cd ~/dendrite/ && git pull && ~/go/bin/dendrite --config dendrite.yaml & disown'
+    [ "$1" ] && su mau -c 'cd ~/maubot/ && source ./bin/activate && pip install --upgrade maubot && python3 -m maubot & disown'
+    [ "$1" ] && su us -c 'cd ~/us.ari.lt/ && source maria.env && source ./venv/bin/activate && git pull && pip install --upgrade -r requirements.txt && ./scripts/run.sh & disown'
+    [ "$1" ] && su voe -c 'cd ~/vim-or-emacs.ari.lt/ && source ./venv/bin/activate && git pull && pip install --upgrade -r requirements.txt && ./scripts/run.sh & disown'
+    [ "$1" ] && su searxng -c 'cd /usr/local/searxng/searxng-src && git pull && pip install --break-system-packages --user -r requirements.txt && SEARXNG_SETTINGS_PATH=/etc/searxng/settings.yml python3 searx/webapp.py & disown'
 }
 
 main
