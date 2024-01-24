8f34dee69718fbcce496bd5c1cea1ef77b4c6e61
Ari Archer <ari.web.xyz@gmail.com>
2024-01-25 00:23:05 +0200

    add searxng to restart script
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -11,6 +11,7 @@ main() {
     su mau -c 'cd ~/maubot/ && source ./bin/activate && python3 -m maubot & disown'
     su us -c 'cd ~/us.ari.lt/ && source maria.env && source ./venv/bin/activate && ./scripts/run.sh & disown'
     su voe -c 'cd ~/vim-or-emacs.ari.lt/ && source ./venv/bin/activate && ./scripts/run.sh & disown'
+    su searxng -c 'cd /usr/local/searxng/searxng-src && SEARXNG_SETTINGS_PATH=/etc/searxng/settings.yml python3 searx/webapp.py & disown'
 }
 
 main
