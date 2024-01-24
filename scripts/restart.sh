eb5ea6529944bfa0fb67d73426c2680d057dc541
Ari Archer <ari.web.xyz@gmail.com>
2024-01-25 00:57:21 +0200

    remove --upgrade freeze in pip
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -8,9 +8,9 @@ main() {
     systemctl restart forgejo
 
     su matrix -c 'cd ~/dendrite/ && git pull && ~/go/bin/dendrite --config dendrite.yaml & disown'
-    [ "$1" ] && su mau -c 'cd ~/maubot/ && source ./bin/activate && pip install --upgrade maubot && python3 -m maubot & disown'
-    [ "$1" ] && su us -c 'cd ~/us.ari.lt/ && source maria.env && source ./venv/bin/activate && git pull && pip install --upgrade -r requirements.txt && ./scripts/run.sh & disown'
-    [ "$1" ] && su voe -c 'cd ~/vim-or-emacs.ari.lt/ && source ./venv/bin/activate && git pull && pip install --upgrade -r requirements.txt && ./scripts/run.sh & disown'
+    [ "$1" ] && su mau -c 'cd ~/maubot/ && source ./bin/activate && pip install maubot && python3 -m maubot & disown'
+    [ "$1" ] && su us -c 'cd ~/us.ari.lt/ && source maria.env && source ./venv/bin/activate && git pull && pip install -r requirements.txt && ./scripts/run.sh & disown'
+    [ "$1" ] && su voe -c 'cd ~/vim-or-emacs.ari.lt/ && source ./venv/bin/activate && git pull && pip install -r requirements.txt && ./scripts/run.sh & disown'
     [ "$1" ] && su searxng -c 'cd /usr/local/searxng/searxng-src && git pull && pip install --break-system-packages --user -r requirements.txt && SEARXNG_SETTINGS_PATH=/etc/searxng/settings.yml python3 searx/webapp.py & disown'
 }
 
